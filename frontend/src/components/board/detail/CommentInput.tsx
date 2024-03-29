27f37d090cb25834f4801daec3e4a073e06607cc
좌소연 <thdus981005@naver.com>
2024-03-29 21:48:22 +0900

    Fix(S10P22A203-216) : 댓글 api 연결 수정

--- frontend/src/components/board/detail/CommentInput.tsx
+++ frontend/src/components/board/detail/CommentInput.tsx
@@ -60,11 +60,10 @@ export default function CommentInput({boardId}:CommentInputProps) {
 
     const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        axiosInstance.post(`/api/board/comment/${boardId}`,
-                {
-                    content
-                }
-            )
+        console.log(content)
+        const formData = new FormData();
+        formData.append('content', content);
+        axiosInstance.post(`/api/board/comment/${boardId}`, formData)
             .then((res : AxiosResponse)=>{console.log(res)})
             .catch(error => {throw new Error(error.message)})
     }
@@ -72,7 +71,7 @@ export default function CommentInput({boardId}:CommentInputProps) {
     return (
         <Wrapper>
             <Form onSubmit={onSubmit}>
-                <Input placeholder='댓글을 입력해주세요.' type ='text' name = "userComment" value = {content} onChange={e=> setContent(e.target.value)}></Input>
+                <Input placeholder='댓글을 입력해주세요.' type ='text' name = "content" value = {content} onChange={e=> setContent(e.target.value)}></Input>
                 <Button type="submit">
                     <Image
                         src = {CommentAdd}
